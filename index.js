3c0305e89a34b11efc9de07638b7cc3d128ebac3
Nicholas Muriithi <nickstech707@gmail.com>
2024-03-27 23:50:39 +0300

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+// This function searches for all matches in an array based on a search string.
+function findMatching(source, sought) {
+    // Returns an array of items that match the search string exactly, ignoring case.
+    return source.filter(
+      (possibleMatch) => possibleMatch.toLowerCase() === sought.toLowerCase()
+    );
+  }
+  
+  // This function performs a fuzzy match on an array based on a test string.
+  function fuzzyMatch(source, testString) {
+    // Returns an array of items that start with the test string, ignoring case.
+    return source.filter(
+      (possibleMatch) =>
+        possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    );
+  }
+  
+  // This function searches for objects in an array where the 'name' property matches the 'soughtName'.
+  function matchName(source, soughtName) {
+    // Returns an array of objects where the 'name' property is exactly equal to 'soughtName'.
+    return source.filter((record) => record.name === soughtName);
+  }
